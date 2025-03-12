c0bc3fddd86dfc145d94817458b455841d67a058
Felice Tempo <f.tempo7@gmail.com>
2025-03-12 09:03:54 -0400

    Converted Date column and handled missing values

--- Capstone_Data_Analysis.py
+++ Capstone_Data_Analysis.py
@@ -1,1 +1,11 @@
-python Capstone_Data_Analysis.py
+import pandas as pd
+maang_path = "C:/Users/Felice/Documents/projects/Capstone/Combined MAANG.csv"
+market_path = "C:/Users/Felice/Documents/projects/Capstone/US Stock Market Data.csv"
+df_maang = pd.read_csv(maang_path)
+df_market = pd.read_csv(market_path)
+print("Datasets loaded successfully!")
+df_maang['Date'] = pd.to_datetime(df_maang['Date'])
+df_market['Date'] = pd.to_datetime(df_market['Date'])
+df_maang.dropna(inplace=True)
+df_market.fillna(method='ffill', inplace=True)
+print("Data cleaning completed!")
